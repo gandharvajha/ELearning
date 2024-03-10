d6647146b9d440fdf205243f3bcbb27ba9f07ea9
gandharvajha <gandharva1371998@gmail.com>
2024-03-10 17:48:33 +0530

    First Commit

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
 </head>
 
 <!-- Start navigation -->
-<nav class="navbar navbar-expand-lg bg-dark">
-  <div class="container-fluid">
+<nav class="navbar navbar-expand-lg bg-dark ">
+  <div class="container-fluid ">
     <a class="navbar-brand" href="index.php">ELearning</a>
     <span class="navbar-text">Learn and Implement</span>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -62,8 +62,241 @@
 <!-- End navigation -->
 
 
+<!-- Start Video background -->
+<div class="conatiner-fluid remove-vid-margin">
+    <div class="vid-parent">
+        <video autoplay muted loop>
+            <source src="video/pexels_videos_1793334.mp4">
+        </video>
+        <div class="vid-overlay"></div>
+        <div class="vid-content">
+            <h1 class="my-content">Welcome to iSchool</h1>
+            <small class="my-content">Learn and Implement</small><br>
+            <a href="#" class="btn btn-danger mt-3">Get Started</a>
+        </div>
+    </div>
+</div>
+<!-- End Video background -->
+
+<!-- Start Text Banner -->
+<div class="container-fluid bg-danger txt-center">
+    <div class="row bottom-banner">
+        <div class="col-sm">
+            <h5><i class="fa-solid fa-book mx-2"></i>100+ Online Courses</h5>
+        </div>
+        <div class="col-sm">
+            <h5><i class="fa-solid fa-users mx-2"></i>Expert Instructors</h5>
+        </div>
+        <div class="col-sm">
+            <h5><i class="fa-solid fa-keyboard mx-2"></i>Lifetime Access</h5>
+        </div>
+        <div class="col-sm">
+            <h5><i class="fa-solid fa-indian-rupee-sign mx-2"></i>Money Back Gaurantee*</h5>
+        </div>
+    </div>
+</div>
+
+<!-- End Text Banner -->
 
 
+<!-- Start Most Popular Course -->
+<div class="conatiner mt-5">
+    <h1 class="text-center">Popular Courses</h1>
+    <!-- Start Most Popular Course 1st Card Deck -->
+   <div>
+   <div class="card-deck mt-4 row">
+        <div class="card col-sm-10 col-md-4 col-xl-4 mt-2">
+            <img src="free-photo-of-top-view-of-decora.jpg" class="card-img-top" alt="Guitar"/>
+            <div class="card-body">
+                <h5 class="card-title">Learn Guitar Easy Way</h5>
+                <p class="card-text">This is some text within a card body.</p>
+            </div>
+            <div class="card-footer">
+                <p class="card-text d-inline">Price: <small><del>&#8377 2000 </del></small>
+                <span class="font-weight-bolder"> &#8377 200</span>
+            </p>
+            <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">
+                 Enroll
+            </a>
+            </div>
+        </div>
+        <div class="card col-sm-10 col-md-4 col-xl-4 mt-2">
+            <img src="free-photo-of-top-view-of-decora.jpg" class="card-img-top" alt="Guitar"/>
+            <div class="card-body">
+                <h5 class="card-title">Learn Guitar Easy Way</h5>
+                <p class="card-text">This is some text within a card body.</p>
+            </div>
+            <div class="card-footer">
+                <p class="card-text d-inline">Price: <small><del>&#8377 2000 </del></small>
+                <span class="font-weight-bolder"> &#8377 200</span>
+            </p>
+            <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">
+                 Enroll
+            </a>
+            </div>
+        </div>
+        <div class="card col-sm-10 col-md-4 col-xl-4 mt-2">
+            <img src="free-photo-of-top-view-of-decora.jpg" class="card-img-top" alt="Guitar"/>
+            <div class="card-body">
+                <h5 class="card-title">Learn Guitar Easy Way</h5>
+                <p class="card-text">This is some text within a card body.</p>
+            </div>
+            <div class="card-footer">
+                <p class="card-text d-inline">Price: <small><del>&#8377 2000 </del></small>
+                <span class="font-weight-bolder"> &#8377 200</span>
+            </p>
+            <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">
+                 Enroll
+            </a>
+            </div>
+        </div>
+        <div class="card col-sm-10 col-md-4 col-xl-4 mt-2">
+            <img src="free-photo-of-top-view-of-decora.jpg" class="card-img-top" alt="Guitar"/>
+            <div class="card-body">
+                <h5 class="card-title">Learn Guitar Easy Way</h5>
+                <p class="card-text">This is some text within a card body.</p>
+            </div>
+            <div class="card-footer">
+                <p class="card-text d-inline">Price: <small><del>&#8377 2000 </del></small>
+                <span class="font-weight-bolder"> &#8377 200</span>
+            </p>
+            <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">
+                 Enroll
+            </a>
+            </div>
+        </div>
+        <div class="card col-sm-10 col-md-4 col-xl-4 mt-2">
+            <img src="free-photo-of-top-view-of-decora.jpg" class="card-img-top" alt="Guitar"/>
+            <div class="card-body">
+                <h5 class="card-title">Learn Guitar Easy Way</h5>
+                <p class="card-text">This is some text within a card body.</p>
+            </div>
+            <div class="card-footer">
+                <p class="card-text d-inline">Price: <small><del>&#8377 2000 </del></small>
+                <span class="font-weight-bolder"> &#8377 200</span>
+            </p>
+            <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">
+                 Enroll
+            </a>
+            </div>
+        </div>
+        <div class="card col-sm-10 col-md-4 col-xl-4 mt-2">
+            <img src="free-photo-of-top-view-of-decora.jpg" class="card-img-top" alt="Guitar"/>
+            <div class="card-body">
+                <h5 class="card-title">Learn Guitar Easy Way</h5>
+                <p class="card-text">This is some text within a card body.</p>
+            </div>
+            <div class="card-footer">
+                <p class="card-text d-inline">Price: <small><del>&#8377 2000 </del></small>
+                <span class="font-weight-bolder"> &#8377 200</span>
+            </p>
+            <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">
+                 Enroll
+            </a>
+            </div>
+        </div>
+
+       
+    </div>
+
+
+   </div>
+    <div class="text-center m-2">
+        <a class="btn btn-danger btn-sm" href="#">View All Course</a>
+    </div>
+</div>
+<!-- End Most Popular Course -->
+
+
+<!-- Start Contact us -->
+<div class="conatiner" id="contact">
+    <h2 class="text-center mb-4">Conatct Us</h2>
+    <div class="row">
+        <div class="col-md-8">
+          <form action="" method="post">
+            <input type="text" class="form-control" name="name" placeholder="Name"><br>
+            <input type="text" class="form-control" name="subject" placeholder="Subject"><br>
+            <input type="emial" class="form-control" name="email" placeholder="E-mail">
+            <textarea name="message" class="form-control" id="" cols="30" rows="4" placeholder="How can i help you?"></textarea><br>
+             <button type="submit" class="btn btn-primary " value="send" name="submit">Submit</button>
+          </form>
+        </div>
+        <div class="col-md-4 stripe text-light">
+            <h4>iSchool</h4>
+            <p>
+                iSchool,
+                Near Police Camp 11,Bakaro,<br/>
+                Jharkhand - 834005<br/>
+                Phone: +000000000<br/>
+            </p>
+        </div>
+    </div>
+</div>
+
+<!-- End Contact us -->
+
+<!-- Start Social Follow -->
+<div class="conatiner-fluid bg-danger">
+    <div class="row text-white text-center p-1">
+        <div class="col-sm">
+            <a class="text-white social-hover" href="#">
+            <i class="fa-brands fa-facebook"></i> Facebook
+            </a>
+        </div>
+        <div class="col-sm">
+            <a class="text-white social-hover" href="#">
+            <i class="fa-brands fa-twitter"></i> Twitter
+            </a>
+        </div>
+        <div class="col-sm">
+            <a class="text-white social-hover" href="#">
+            <i class="fa-brands fa-whatsapp"></i> Whatsapp
+            </a>
+        </div>
+        <div class="col-sm">
+            <a class="text-white social-hover" href="#">
+            <i class="fa-brands fa-instagram"></i> Instagram
+            </a>
+        </div>
+    </div>
+</div>
+<!-- End Social Follow -->
+
+<!-- Start Footer  -->
+
+<div class="conatiner-fluid p-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <h5>About Us</h5>
+                <p>
+                iSchool,
+                Near Police Camp 11,Bakaro,<br/>
+                Jharkhand - 834005<br/>
+                Phone: +000000000<br/>
+                </p>
+            </div>
+            <div class="col-sm">
+                <h5>Category</h5>
+                <a class="text-dark" href="#">Web Development</a><br/>
+                <a class="text-dark" href="#">Web Designing</a><br/>
+                <a class="text-dark" href="#">Andriod App Dev</a><br/>
+                <a class="text-dark" href="#">ios Development</a><br/>
+                <a class="text-dark" href="#">Data Analysis</a><br/>
+            </div>
+            <div class="col-sm">
+                <h5>Conatct Us</h5>
+                <p>
+                iSchool,
+                Near Police Camp 11,Bakaro,<br/>
+                Jharkhand - 834005<br/>
+                Phone: +000000000<br/>
+            </p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Footer  -->
 
 <body>
 <!-- jquery    -->
