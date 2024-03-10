a4d2378d01ccf38a8e59d0421b97d74d4aabba8d
gandharvajha <gandharva1371998@gmail.com>
2024-03-10 13:49:32 +0530

    first commit

--- index.php
+++ index.php
@@ -4,8 +4,73 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ELearning</title>
+    <!-- Bootstrap css link -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Fontawasome css -->
+    <link href="css/all.min.js" rel="stylesheet">
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,200;1,300;1,400;1,500;1,700;1,800;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <!-- custome css -->
+    <link href="css/style.css" rel="stylesheet">
+  </head>
 </head>
+
+<!-- Start navigation -->
+<nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">ELearning</a>
+    <span class="navbar-text">Learn and Implement</span>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse px-5" id="navbarNav">
+      <ul class="navbar-nav custom-nav">
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Courses</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Payment</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">My Profile</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Logout</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Signup</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Feedback</a>
+        </li>
+        <li class="nav-item custom-nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- End navigation -->
+
+
+
+
+
 <body>
-    
+<!-- jquery    -->
+<script src="js/jquery-3.7.1.min.js"></script>
+ <!-- Bootstrap js link -->
+<script src="js/bootstrap.bundle.min.js"></script>
+ <!-- Fontawasome js -->
+ <script src="js/all.min.js"></script>
 </body>
 </html>
